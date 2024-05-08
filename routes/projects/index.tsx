bb2ea09956ffdac582842c7be165532ca3d08864
Empa <emanuel@empa.xyz>
2024-05-08 11:57:52 +0200

    More project stuff

--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -4,6 +4,7 @@ type Project = {
   href: string;
   imageUrl: string;
   date: string;
+  github?: string;
 };
 
 const projects: Project[] = [
@@ -12,36 +13,57 @@ const projects: Project[] = [
     description:
       "Saft is a modern, high-level programming language designed for simplicity, efficiency, and expressiveness. It draws inspiration from functional and imperative paradigms, aiming to provide a clean and intuitive syntax for developers",
     href: "/projects/saft",
-    imageUrl: "/img/saft_article.jpg",
-    date: "2024-05-07",
+    imageUrl: "/img/saft_article.jpeg",
+    date: "2023-12-22 - now",
+    github: "https://github.com/Quaqqer/saft",
   },
   {
-    name: "Saft2",
+    name: "Chip8",
     description:
-      "Saft is a modern, high-level programming language designed for simplicity, efficiency, and expressiveness. It draws inspiration from functional and imperative paradigms, aiming to provide a clean and intuitive syntax for developers",
-    href: "/projects/saft",
-    imageUrl: "",
-    date: "2024-05-07",
+      "A javascript implementation of a CHIP-8 emulator. CHIP-8 is an imaginary console, a popular introduction to the world of programming emulators",
+    href: "/projects/chip8",
+    imageUrl: "/img/chip8_article.jpeg",
+    date: "",
+    github: "https://github.com/Quaqqer/empa.xyz/tree/trunk/lib/projects/chip8",
   },
   {
-    name: "Saft",
+    name: "Tetris",
+    description: "The game of tetris coded up in a single evening",
+    href: "/projects/tetris",
+    imageUrl: "/img/tetris_article.jpeg",
+    date: "",
+    github: "https://github.com/Quaqqer/empa.xyz/tree/trunk/lib/projects/chip8",
+  },
+  {
+    name: "Cinemraft",
     description:
-      "Saft is a modern, high-level programming language designed for simplicity, efficiency, and expressiveness. It draws inspiration from functional and imperative paradigms, aiming to provide a clean and intuitive syntax for developers",
-    href: "/projects/saft",
-    imageUrl: "/img/saft_article.jpg",
-    date: "2024-05-07",
+      "A simple voxel world generation with 3d rendering for the browser using three.js",
+    href: "/projects/cinemraft",
+    imageUrl: "/img/cinemraft_article.jpeg",
+    date: "",
+    github:
+      "https://github.com/Quaqqer/empa.xyz/tree/trunk/lib/projects/cinemraft",
+  },
+  {
+    name: "Snake",
+    description:
+      "A simple implementation of the snake game, with a leaderboard",
+    href: "/projects/snake",
+    imageUrl: "/img/snake_article.jpeg",
+    date: "",
+    github: "https://github.com/Quaqqer/empa.xyz/tree/trunk/lib/projects/snake",
+  },
+  {
+    name: "Game of Life",
+    description: "Conways game of life is popular cellular automata",
+    href: "/projects/gol",
+    imageUrl: "/img/gol_article.jpeg",
+    date: "",
+    github: "https://github.com/Quaqqer/empa.xyz/tree/trunk/lib/projects/gol",
   },
 ];
 
 export default function Page() {
-  return (
-    <div>
-      <Example />
-    </div>
-  );
-}
-
-function Example() {
   return (
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -51,7 +73,7 @@ function Example() {
           </h2>
         </div>
 
-        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-3 items-start">
           {projects.map((p, i) => (
             <a
               href={p.href}
@@ -69,12 +91,10 @@ function Example() {
                     {p.name}
                   </h3>
 
-                  <time dateTime={p.date} className="text-gray-500">
-                    {p.date}
-                  </time>
+                  <div className="text-gray-500">{p.date}</div>
                 </div>
 
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-400 group-hover:text-gray-300">
+                <p className="mt-5 text-sm leading-6 text-gray-400 group-hover:text-gray-300">
                   {p.description}
                 </p>
               </div>
