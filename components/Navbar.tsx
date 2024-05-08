b44f3ab6a19aef036022934a5835c711fee199ae
Empa <emanuel@empa.xyz>
2024-05-09 00:32:49 +0200

    Underline navbar, 404 page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ export function Navbar({ currentHref }: { currentHref: string }) {
                   href={href}
                   class={clsx(
                     "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white",
-                    current && "bg-gray-900 text-white",
+                    current && "bg-gray-900 text-white underline",
                   )}
                 >
                   {name}
--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,22 +6,28 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">The page you were looking for doesn't exist.</p>
-          <a href="/" class="underline">
-            Go back home
-          </a>
+
+      <main className="grid min-h-full items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-400">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-300 sm:text-5xl">
+            Page not found
+          </h1>
+
+          <p className="mt-6 text-base leading-7 text-gray-400">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+
+          <div class="mt-10">
+            <a
+              href="/"
+              className="rounded-md bg-indigo-600/80 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600/80"
+            >
+              Go back home
+            </a>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
