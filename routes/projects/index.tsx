2760da44fbb6d02ab3c3318a76b6a544e2bd3359
Empa <emanuel@empa.xyz>
2024-05-21 15:05:01 +0200

    Fix punctuation

--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -17,7 +17,7 @@ const projects: Project[] = [
   {
     name: "Saft",
     description:
-      "Saft is my personal programming language project. I've created a lexer, parser, compiler and bytecode vm in this project",
+      "Saft is my personal programming language project. I've created a lexer, parser, compiler and bytecode vm in this project.",
     imageUrl: "/img/saft_article.jpeg",
     date: "2023-12-22 - now",
     href: "https://github.com/Quaqqer/saft",
@@ -26,7 +26,7 @@ const projects: Project[] = [
   {
     name: "Nemu",
     description:
-      "A work-in-progress NES emulator. Currently the CPU is pretty complete, and graphics is the current focus",
+      "A work-in-progress NES emulator. Currently the CPU is pretty complete, and graphics is the current focus.",
     imageUrl: "/img/nemu_article.jpeg",
     date: "2023-06-05 - now",
     href: "https://github.com/Quaqqer/nemu",
@@ -35,7 +35,7 @@ const projects: Project[] = [
   {
     name: "Advent of Code",
     description:
-      "I have participated in Advent of Code since the year 2019. I'm one of those people who wake up at 6 am. to finish my puzzles as quickly as possible, before going back to sleep",
+      "I have participated in Advent of Code since the year 2019. I'm one of those people who wake up at 6 am. to finish my puzzles as quickly as possible, before going back to sleep.",
     imageUrl: "/img/aoc_article.jpeg",
     date: "2019-12-01 - now",
     href: "https://github.com/Quaqqer/aoc",
@@ -43,7 +43,7 @@ const projects: Project[] = [
   },
   {
     name: "Tetris",
-    description: "The game of tetris coded up in a single evening",
+    description: "The game of tetris coded up in a single evening.",
     imageUrl: "/img/tetris_article.jpeg",
     date: "2024-01-31",
     href: "/projects/tetris",
@@ -53,7 +53,7 @@ const projects: Project[] = [
   },
   {
     name: "CHIP-8 in JS",
-    description: "A javascript implementation of a CHIP-8 emulator",
+    description: "A javascript implementation of a CHIP-8 emulator.",
     imageUrl: "/img/chip8_article.jpeg",
     date: "2024-01-28",
     href: "/projects/chip8",
@@ -64,7 +64,7 @@ const projects: Project[] = [
   {
     name: "CHIP-8 in Rust",
     description:
-      "A CHIP-8 emulator written in rust. CHIP-8 is an imaginary console, a popular introduction to the world of programming emulators",
+      "A CHIP-8 emulator written in rust. CHIP-8 is an imaginary console, a popular introduction to the world of programming emulators.",
     imageUrl: "/img/chip8_rs_article.png",
     imageProps: { style: { imageRendering: "pixelated" } },
     date: "2023-06-04",
@@ -74,7 +74,7 @@ const projects: Project[] = [
   {
     name: "Cinemraft",
     description:
-      "A simple voxel world generation with 3d rendering for the browser using three.js",
+      "A simple voxel world generation with 3d rendering for the browser using three.js.",
     imageUrl: "/img/cinemraft_article.png",
     date: "2022-10-28",
     href: "/projects/cinemraft",
@@ -85,7 +85,7 @@ const projects: Project[] = [
   {
     name: "Game of Life",
     description:
-      "Conways game of life is popular cellular automata. I implemented it in some basic JavaScript for fun",
+      "Conways game of life is popular cellular automata. I implemented it in some basic JavaScript for fun.",
     imageUrl: "/img/gol_article.png",
     date: "2022-05-18",
     href: "/projects/gol",
@@ -95,7 +95,7 @@ const projects: Project[] = [
   },
   {
     name: "Snake",
-    description: "A simple implementation of the snake game",
+    description: "A simple implementation of the snake game.",
     imageUrl: "/img/snake_article.png",
     date: "2022-05-17",
     href: "/projects/snake",
