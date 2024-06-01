fe2fc412439b3938cf722a96ffc8f8dd6adc6f16
Empa <emanuel@empa.xyz>
2024-06-01 23:52:43 +0200

    Add padding to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { JSX } from "preact";
 
 export default function Footer(): JSX.Element {
   return (
-    <footer class="bg-gray-900 text-gray-400 text-sm">
+    <footer class="bg-gray-900 text-gray-400 text-sm px-5">
       <div class="max-w-xl mx-auto py-8">Copyright © 2024 Quaqqer</div>
     </footer>
   );
